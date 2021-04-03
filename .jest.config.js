e5852fcda1460a423e41ea157b61644609092b80
vitorregisr <vitorregisrr@gmail.com>
2021-04-03 10:58:36 -0300

    Add Jest and Babel config and packages

---
+++ .jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnviroment: 'jsdom',
+    testPathIgnorePatterns: [
+        '/node_modules/', '/.next/'
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts(x)'],
+    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
