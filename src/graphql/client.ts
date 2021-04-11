8b68ea4eadf13cf552028d248ccd25d0b2795cb5
vitorregisr <vitorregisrr@gmail.com>
2021-04-11 20:02:24 -0300

    Fix some Graphql and API bugs

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = process.env.GARAPHQL_HOST || ''
+const endpoint = process.env.GRAPHQL_HOST || ''
 
 const client = new GraphQLClient(endpoint, {
   headers: {
