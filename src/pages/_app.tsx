11974bfd3a49a63164b32b37cc4570d2c5d45452
vitorregisr <vitorregisrr@gmail.com>
2021-04-21 15:02:40 -0300

    Add Google Search Console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,10 @@ function App({ Component, pageProps }: AppProps) {
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin=""
         />
+        <meta
+          name="google-site-verification"
+          content="HYb0LBHuY2FMcpYcET1w6V0LjpKdnkDiLHg0CBhNHfQ"
+        />
       </Head>
       <DefaultSeo {...SEO} />
       <GlobalStyles />
